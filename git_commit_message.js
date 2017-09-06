be32edbca8917d7143c9d40bcf0341a50c80a217
Ridham Tarpara <ridham.tarpara@gmail.com>
2017-09-06 16:23:13 +0530

    branchname parsed

--- git_commit_message.js
+++ git_commit_message.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-const shell = require("shelljs");
+const shell = require('shelljs');
 const chalk = require('chalk');
 
 const args = process.argv.slice(2);
@@ -7,8 +7,12 @@ console.log(args);
 if (!args.length) {
   console.log(chalk.red('No commit message provided'));
 } else {
-  let result = shell.exec(`git add -A . && git commit -a -m '${args[0]}'`);
+  const result = shell.exec(`git add -A . && git commit -a -m '${args[0]}'`);
   if (!result.stderr) {
+    const index = result.indexOf(']');
+    const branchName = result.substr(1, result.indexOf(' ') - 1);
+    console.log({ branchName });
+
     console.log(chalk.green(result.stdout));
   } else {
     console.log(chalk.red(result.stderr));
