7fa7e0efbe96fe2d8c37544f47980b59a4c83177
Ridham Tarpara <ridham.tarpara@gmail.com>
2017-09-07 14:05:36 +0530

    color changed

--- git_commit_message.js
+++ git_commit_message.js
@@ -7,9 +7,9 @@ const args = process.argv.slice(2);
 
 const getSpaceDelimitedValue = (string, initIndex) => string.substr(initIndex || 0, string.indexOf(' '));
 
-const getFileChangedMessage = commitDetails => `${chalk.brightBlue('Files Changed\t: ', getSpaceDelimitedValue(commitDetails[0]))}`;
-const getInsertMessage = commitMessage => `${chalk.green('Insertions\t: ', getSpaceDelimitedValue(commitMessage))}`;
-const getDeleteMessage = commitMessage => `${chalk.red('Deletions\t: ', getSpaceDelimitedValue(commitMessage))}`;
+const getFileChangedMessage = commitDetails => `${chalk.blueBright('Files Changed\t: ', getSpaceDelimitedValue(commitDetails[0]))}`;
+const getInsertMessage = commitMessage => `${chalk.greenBright('Insertions\t: ', getSpaceDelimitedValue(commitMessage))}`;
+const getDeleteMessage = commitMessage => `${chalk.redBright('Deletions\t: ', getSpaceDelimitedValue(commitMessage))}`;
 const getInsertAndDeleteMessage = commitDetails => `${getInsertMessage(commitDetails[1])}\n${getDeleteMessage(commitDetails[2])}`;
 
 const getChangesMessage = (commitDetails, commitDetailLength) => {
@@ -25,13 +25,18 @@ const getChangesMessage = (commitDetails, commitDetailLength) => {
 if (!args.length) {
   console.log(chalk.red('No commit message provided'));
 } else {
+	// exa. [ft/commit d525baa] color changed\n 1 file changed, 1 insertion(+)\n
   const result = shell.exec(`git add -A . && git commit -a -m '${args[0]}'`);
   let output = '';
-  if (!result.stderr) {
+  console.log(result);
+  if (!result.stderr && !result.code) {
+		// get the branch name
     const branchName = getSpaceDelimitedValue(result, 1);
+		// attach branch name to output
     output = `${output}${chalk.bold('Branch name\t: ', branchName)}\n`;
-    const newLineIndex = result.indexOf('\n ');
-    const commitDetails = result.substr(newLineIndex + 2).split(/, |\n /);
+		// commitDetails includes additions, deletions, file added, file removed, renamed etc.
+		// starts from \n and have space before every line
+    const commitDetails = result.substr(result.indexOf('\n ') + 2).split(/, |\n /);
     const commitDetailLength = commitDetails.length;
     console.log({ commitDetails, commitDetailLength });
     switch (commitDetailLength) {
@@ -39,6 +44,7 @@ if (!args.length) {
         output = `${output}${chalk.bold(commitDetails[0].sunstr(0, commitDetails[0].indexOf(' ')))}`;
         console.log(chalk`${output}`);
         break;
+			// If only addition and deletions
       case 2:
       case 3:
         output = `${output}${getFileChangedMessage(commitDetails)}\n${getChangesMessage(commitDetails, commitDetailLength)}`;
