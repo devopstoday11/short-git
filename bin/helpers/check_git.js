50388578d47289638dfa7c8d8d5b5d640cfdb414
Ridham Tarpara <ridham.tarpara@gmail.com>
2017-09-08 12:16:23 +0530

    git fallback added

--- bin/helpers/check_git.js
+++ bin/helpers/check_git.js
@@ -3,7 +3,8 @@ const chalk = require('chalk');
 
 module.expoerts = () => {
   console.log(1);
-  if (!shell.which('gitdsdf')) {
+  console.log(shell.which('sdasdas'));
+  if (!shell.which('sdasdas')) {
     console.log(2);
     shell.echo(`${chalk.bold.redBright('Git is not installed')}`);
     return false;
