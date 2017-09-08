851b46863a0daa3b1e2b56ea3df71df041db1a76
Ridham Tarpara <ridham.tarpara@gmail.com>
2017-09-08 17:33:33 +0530

    push errorlog formatted

--- bin/git/git_push.js
+++ bin/git/git_push.js
@@ -26,7 +26,7 @@ module.exports = (branchName, args, callback) => {
     if (stderr) {
       if (stderr.indexOf('rejected' !== -1)) {
         const output = 'Updates were rejected because the remote contains work that you do not have locally. This is usually caused by another repository pushing to the same ref. You may want to first integrate the remote changes (e.g., \'git pull ...\') before pushing again.';
-        callback(`${chalk.red.bold('Push failed')}\n${chalk.redBright(output)}`);
+        callback(`${chalk.red('Push failed')}\n${chalk.redBright(output)}`);
         return;
       }
       // get hash
