9ae2336998e0f22a936a177e760f9aa720ba570b
Ridham Tarpara <ridham.tarpara@gmail.com>
2017-09-08 16:39:45 +0530

    invalid arg removed from data

--- bin/git/git_push.js
+++ bin/git/git_push.js
@@ -25,12 +25,8 @@ module.exports = (branchName, args, callback) => {
     // console.log(121, { code, stdout, stderr });
     if (stderr) {
       console.log(2);
-      const commitLine = stderr.substr(stderr.indexOf('..') + 2);
+      const commitLine = stderr.substr(stderr.indexOf('..') + 2).substr(0, stderr.indexOf('  '));
       console.log({ commitLine });
-      console.log(branchName);
-      console.log(stderr.indexOf(branchName) - 2);
-      const commitLine1 = commitLine.substr(0, stderr.indexOf(branchName) - 2);
-      console.log({ commitLine1 });
       callback(chalk.redBright(stderr));
       return;
     }
