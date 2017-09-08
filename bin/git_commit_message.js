22605ab4318c647677f409d631178b8a3d37ba22
Ridham Tarpara <ridham.tarpara@gmail.com>
2017-09-08 12:21:23 +0530

    git fallback added

--- bin/git_commit_message.js
+++ bin/git_commit_message.js
@@ -99,12 +99,14 @@ if (!result.stderr && !result.code) {
   const commitDetailLength = commitDetails.length;
   output = `${output}${getFileChangedMessage(commitDetails)}${getChangesMessage(commitDetails, commitDetailLength)}${getFilesMessage(commitDetails.slice(2))}`;
   console.log(output);
+  shell.exit(1);
 } else {
   // nothing to commit
   if (result.indexOf('nothing to commit')) {
     output = `${output}${getBranchName(result.substr(10, result.indexOf('\n') - 10))}\n`;
     output = `${output}${chalk.redBright('Nothing to commit')}`;
     console.log(output);
+    shell.exit(1);
   }
   console.log(chalk.redBright(result.stderr));
 }
