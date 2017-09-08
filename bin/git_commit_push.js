ecb15feb93c6aba3c576eed1c32fd423a9782251
Ridham Tarpara <ridham.tarpara@gmail.com>
2017-09-08 14:18:01 +0530

    Auto commit

--- bin/git_commit_push.js
+++ bin/git_commit_push.js
@@ -3,7 +3,7 @@ const shell = require('shelljs');
 const chalk = require('chalk');
 const getCurrentBranch = require('./helpers/get_current_branch');
 // SET SILENT TRUE SO THAT DEFAULT OUTPUT IS NOT PRINTED ON CONSOLE
-shell.config.silent = true;
+// shell.config.silent = true;
 // get command line argument for commit message
 const args = process.argv.slice(2);
 const argsString = args.join(' ');
